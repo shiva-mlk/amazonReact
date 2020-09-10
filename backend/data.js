e69385f912c48b14e53ffcbf711704cc29f06786
shiva <shivamaleki112@yahoo.com>
2020-09-11 02:11:56 +0430

    first commit

---
+++ backend/data.js
@@ -0,0 +1,38 @@
+export default{
+    products:[{
+        _id: '1',
+        name: 'shirt',
+        category: 'shirt' ,
+        image: '/images/number1.jpg',
+        price: 90,
+        brand: 'nike',
+        rating: 4.2,
+        numReviews: 10,
+        countInStock:5
+    },
+    {
+        _id: '2',
+        name: ' slim shirt',
+        category: 'shirt' ,
+        image: '/images/number1.jpg',
+        price: 30,
+        brand: 'nike',
+        rating: 3,
+        numReviews: 10,
+        countInStock:6
+    },
+    {
+        _id: '3',
+        name: 'pants',
+        category: 'psnts' ,
+        image: '/images/number1.jpg',
+        price: 40,
+        brand: 'nike',
+        rating: 5,
+        numReviews: 10,
+        countInStock:6
+    }
+
+]
+
+}
---
+++ backend/routes/userRoute.js
@@ -0,0 +1,44 @@
+import express, { Router } from 'express'
+import User from '../models/userModel'
+import { getToken } from '../util'
+
+const router = express.Router()
+
+router.post("/signin", async(req, res)=>{
+    const signinUser = await User.findOne({
+        email: req.body.email,
+        password: req.body.password
+    }
+    )
+    if (signinUser){
+        res.send({
+            _id:signinUser.id,
+            name: signinUser.name,
+            email: signinUser.email,
+            isAdmin: signinUser.isAdmin,
+            token: getToken(signinUser)
+
+        })
+    }else{
+        res.status(401).send({msg: 'Invalid username or password'})
+    }
+})
+
+router.get("/createadmin", async(req, res)=>{
+    try {
+        const user = new User({
+            name: 'shiva',
+            email: 'shivamaleki112@yahoo.com',
+            password: '1234',
+            isAdmin: true
+        })
+
+        const newUser = await user.save()
+        res.send(newUser)
+    } catch (error) {
+        res.send({msg: error.message})
+        
+    }
+})
+
+export default router
