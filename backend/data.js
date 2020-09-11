405c9fa5e0137a8beb7d3c71b689f5ced5c6c97f
shiva <shivamaleki112@yahoo.com>
2020-09-11 11:42:01 +0430

    second commit

--- backend/data.js
+++ backend/data.js
@@ -14,7 +14,7 @@ export default{
         _id: '2',
         name: ' slim shirt',
         category: 'shirt' ,
-        image: '/images/number1.jpg',
+        image: 'https://www.politix.com.au/dw/image/v2/ABBA_PRD/on/demandware.static/-/Sites-politix-master-catalog/default/dwa976fc4d/images/hires/Winter19/ss05-navy-1.jpg?sw=2000&sh=2000&sm=fit',
         price: 30,
         brand: 'nike',
         rating: 3,
@@ -23,15 +23,49 @@ export default{
     },
     {
         _id: '3',
+        name: ' slim shirt',
+        category: 'shirt' ,
+        image: 'https://ae01.alicdn.com/kf/HTB1.ub.BlyWBuNkSmFPq6xguVXal/New-Fashion-Casual-Men-Shirt-Long-Sleeve-Mandarin-Collar-Slim-Fit-Shirt-Men-Korean-Business-Mens.jpg',
+        price: 30,
+        brand: 'nike',
+        rating: 3,
+        numReviews: 10,
+        countInStock:6
+    },
+    {
+        _id: '4',
         name: 'pants',
-        category: 'psnts' ,
-        image: '/images/number1.jpg',
+        category: 'pants' ,
+        image: 'https://www.dickies.com/dw/image/v2/AAYI_PRD/on/demandware.static/-/Sites-master-catalog-dickies/default/dw87aa282d/images/main/WP803_BK_FR.jpg?sw=583&sh=746&sm=fit',
         price: 40,
         brand: 'nike',
         rating: 5,
         numReviews: 10,
         countInStock:6
-    }
+    },
+
+    {
+        _id: '5',
+        name: 'pants',
+        category: 'pants' ,
+        image: 'https://www.dickies.com/dw/image/v2/AAYI_PRD/on/demandware.static/-/Sites-master-catalog-dickies/default/dwd841f8f9/images/main/WP811_BK_FR.jpg?sw=583&sh=746&sm=fit',
+        price: 40,
+        brand: 'nike',
+        rating: 5,
+        numReviews: 10,
+        countInStock:6
+    },
+    {
+    _id: '6',
+    name: 'pants',
+    category: 'pants' ,
+    image: 'https://waltersclothing.com/wp-content/uploads/2019/11/71tLYrRKRmL._UX466_.jpg',
+    price: 40,
+    brand: 'nike',
+    rating: 5,
+    numReviews: 10,
+    countInStock:6
+}
 
 ]
 
